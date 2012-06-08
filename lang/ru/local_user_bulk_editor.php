3df2c52f7cb784adb6989b04460ec6dac67db0d4
Andrew Kama <kamasutra12@yandex.ru>
2012-06-08 15:17:43 +0400

    Update translation

---
+++ lang/ru/local_user_bulk_editor.php
@@ -0,0 +1,17 @@
+<?php
+
+$string['pluginname'] = 'Редактор пользователей';
+$string['userbulkeditor'] = 'Редактор пользователей';
+$string['ubfieldseditoraction'] = 'Применение изменений . . .';
+$string['ubfieldseditor'] = 'Редактор пользователей';
+$string['selectfield'] = 'Выберите поле профиля';
+$string['nofiled'] = '<div style="margin: 3px; padding: 5px; border: 1px solid #b7b7b7; background-color: #e7e7e7">Поле профиля не выбрано.</div>';
+$string['selectedfield'] = '<div style="margin: 3px; padding: 5px; border: 1px solid #b7b7b7; background-color: #e7e7e7">Выбранное поле: {$a}.</div>';
+$string['updatefield'] = 'Обновление поля <b>{$a->f}</b>. Новое значение: <b>{$a->n}</b><br>';
+$string['check'] = 'Отметить';
+$string['uncheck'] = 'Снять отметку';
+$string['reverse'] = 'Инверсия';
+$string['replaceall'] = 'Заменить все';
+$string['replace'] = 'Замена {$a}';
+$string['to'] = 'на';
+
--- version.php
+++ version.php
@@ -4,3 +4,4 @@ $plugin->version  = 2012060815;   // The (date) version of this plugin
 $plugin->requires = 2011120100;   // Requires this Moodle version
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '0.2';
+
