84169f80d58afdb49097f88c98506436ea746e86
kamat <kamasutra12@yandex.ru>
2012-06-09 11:31:08 +0400

    Update master

--- lang/en/local_user_bulk_editor.php
+++ lang/en/local_user_bulk_editor.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * User bulk editor local plugin version information
+ *
+ * @package    local
+ * @subpackage user_bulk_editor
+ * @copyright  2012 Andrew "Kama" (kamasutra12@yandex.ru) 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['pluginname'] = 'UBEditor';
 $string['userbulkeditor'] = 'UBEditor';
 $string['ubfieldseditoraction'] = 'Updating data . . .';
