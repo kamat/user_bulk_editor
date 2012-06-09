a81defdea8da0507bae5b3387931b187d792e193
kamat <kamasutra12@yandex.ru>
2012-06-09 11:30:50 +0400

    Update master

--- version.php
+++ version.php
@@ -1,7 +1,16 @@
 <?php
 
+/**
+ * User bulk editor local plugin version information
+ *
+ * @package    local
+ * @subpackage user_bulk_editor
+ * @copyright  2012 Andrew "Kama" (kamasutra12@yandex.ru) 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $plugin->version  = 2012060815;   // The (date) version of this plugin
 $plugin->requires = 2011120100;   // Requires this Moodle version
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.2';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '0.3';
 
