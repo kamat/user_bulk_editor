a7901e5c62b007ede4c76fe3522d27fc5be59fe0
kamat <kamasutra12@yandex.ru>
2012-07-19 11:22:31 +0400

    Update version.php

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
  */
 
 $plugin->version  = 2012062915;   // The (date) version of this plugin
-$plugin->requires = 2011120100;   // Requires this Moodle version
+$plugin->requires = 2011120500;   // Requires this Moodle version
 $plugin->maturity = MATURITY_RC;
 $plugin->release = '0.5';
